084c13236e0dfeda0e73f6e4dd8a0e21fc9e5fb3
silent_mantra <silentmantra.ru@gmail.com>
2026-01-18 15:38:58 +0200

    fix item.settings proxy

--- assets/ru-BlGp1eXc.js
+++
@@ -1,1 +0,0 @@
-const t={width:"ширина",gap:"расстояние"};export{t as default};
---
+++ assets/ru-DKh8Ko3x.js
@@ -0,0 +1,1 @@
+const e={width:"ширина",gap:"расстояние",wheelup:"колесо вверх",wheeldown:"колесо вниз"};export{e as default};
