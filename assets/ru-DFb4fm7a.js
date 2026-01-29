7439677088b486bff54b99a5258297a3d4f1a914
silent_mantra <silentmantra.ru@gmail.com>
2026-01-29 18:21:52 +0200

    tweak status bar

---
+++ assets/ru-DFb4fm7a.js
@@ -0,0 +1,1 @@
+const e={width:"ширина",gap:"расстояние",wheelup:"колесо вверх",wheeldown:"колесо вниз","stereo reader":"СТЕРЕО ЧТЕНИЕ","install app":"установить приложение"};export{e as default};
--- assets/ru-DKh8Ko3x.js
+++
@@ -1,1 +0,0 @@
-const e={width:"ширина",gap:"расстояние",wheelup:"колесо вверх",wheeldown:"колесо вниз"};export{e as default};
