f3f91c3b73fa6385d0a73f7caa62183a8b63b43d
silent_mantra <silentmantra.ru@gmail.com>
2026-02-13 17:50:38 +0200

    add pwa vite plugin

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/app/service-worker.js', { scope: '/app/' })})}
--- sw-register.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/app/service-worker.js")
-      .then(() => console.log("✅ Service Worker registered"))
-      .catch((err) => console.error("SW registration failed:", err));
-  });
-}
