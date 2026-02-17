351e67bfa7b199297d717091184a0e51df74fcff
silent_mantra <silentmantra.ru@gmail.com>
2026-02-17 14:25:10 +0200

    return and fix chunk gap in row height

--- assets/ItemItems-CpUrs7kt.js
+++ assets/ItemItems-CpUrs7kt.js
@@ -1,4 +1,4 @@
-import{I as i,t as o,f as l,e as u,C as h,h as c,g as f}from"./index-v3nUAa20.js";class m extends h{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[c("div",{innerHTML:"&nbsp;",style:`
+import{I as i,t as o,f as l,e as u,C as h,h as c,g as f}from"./index-DRcpsH5d.js";class m extends h{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[c("div",{innerHTML:"&nbsp;",style:`
                 background:url(${this.$item.thumbnailUrl}) no-repeat top right;
                 background-size: auto 100%;
                 position:absolute;
