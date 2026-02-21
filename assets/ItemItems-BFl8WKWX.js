8c68a4340b4353dd2257cba3d547243354ef63df
silent_mantra <silentmantra.ru@gmail.com>
2026-02-21 10:47:40 +0200

    fix settings dialog import.meta.glob to search item settings

--- assets/ItemItems-BFl8WKWX.js
+++ assets/ItemItems-BFl8WKWX.js
@@ -1,4 +1,4 @@
-import{I as i,f as o,e as l,C as u,g as h}from"./App-CXLwFnP_.js";import{C as c,m as f}from"./index-BkzqfRJK.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
+import{I as i,f as o,e as l,C as u,g as h}from"./App-OwZbZFNx.js";import{C as c,m as f}from"./index-DK3zVhyu.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
                 background:url(${this.$item.thumbnailUrl}) no-repeat top right;
                 background-size: auto 100%;
                 position:absolute;
