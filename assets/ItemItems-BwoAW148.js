8a9fa2069c7699834adb31325f38f258d0ae9ef9
silent_mantra <silentmantra.ru@gmail.com>
2026-02-22 15:00:44 +0200

    add image demo files

--- assets/ItemItems-BwoAW148.js
+++ assets/ItemItems-BwoAW148.js
@@ -1,4 +1,4 @@
-import{I as i,f as o,e as l,C as u,g as h}from"./App-2iPQQnsw.js";import{C as c,m as f}from"./index-CxUbm13g.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
+import{I as i,f as o,e as l,C as u,g as h}from"./App-CDSzeW__.js";import{C as c,m as f}from"./index-CHR6cbVc.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
                 background:url(${this.$item.thumbnailUrl}) no-repeat top right;
                 background-size: auto 100%;
                 position:absolute;
---
+++ assets/aerial-view-greenery-forest-fit-DPW3hLNG.js
@@ -0,0 +1,1 @@
+const e="/app/assets/aerial-view-greenery-forest-fit-C-fgHEAr.jpg";export{e as default};
