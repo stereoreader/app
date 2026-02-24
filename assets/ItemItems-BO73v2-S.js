5a7cc51733a2f0955848087179d7329a518152dd
silent_mantra <silentmantra.ru@gmail.com>
2026-02-24 15:45:56 +0200

    optimize line calc

--- assets/ItemItems-BO73v2-S.js
+++ assets/ItemItems-BO73v2-S.js
@@ -1,4 +1,4 @@
-import{I as i,f as o,e as l,C as u,g as h}from"./App-CDSzeW__.js";import{C as c,m as f}from"./index-CHR6cbVc.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
+import{I as i,f as o,e as l,C as u,g as h}from"./App-4rGboe-G.js";import{C as c,m as f}from"./index-CZwuOgZ6.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
                 background:url(${this.$item.thumbnailUrl}) no-repeat top right;
                 background-size: auto 100%;
                 position:absolute;
