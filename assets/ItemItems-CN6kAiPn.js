149e2f07c9367fdbece6358b0896694db72dd5ed
silent_mantra <silentmantra.ru@gmail.com>
2026-02-26 18:42:50 +0200

    fix listening timeout

--- assets/ItemItems-CN6kAiPn.js
+++ assets/ItemItems-CN6kAiPn.js
@@ -1,4 +1,4 @@
-import{I as i,f as o,e as l,C as u,g as h}from"./App-DsCYFdwE.js";import{C as c,m as f}from"./index-CBxF4WYM.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
+import{I as i,f as o,e as l,C as u,g as h}from"./App-QmdIbyCe.js";import{C as c,m as f}from"./index-Cy7zJnKg.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
                 background:url(${this.$item.thumbnailUrl}) no-repeat top right;
                 background-size: auto 100%;
                 position:absolute;
